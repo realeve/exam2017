6fa5270001f39211dd02091b5d3ec444b029548b
realeve@qq.com <realeve@qq.com>
2018-06-09 23:42:06 +0800

    细节调整

--- src/pages/Score.vue
+++ src/pages/Score.vue
@@ -2,22 +2,21 @@
   <div>
     <v-header/>
     <div class="content">
-      <h3>1.各部门平均分与参与率</h3>
-      <ul class="dept-rate">
-        <li v-for="({avg_score,rate,user_dpt},i) in depts" :key="i">
-          <span>{{i+1}}.{{user_dpt}}</span>
-          <span>{{avg_score}}分</span>
-          <span>{{rate}}%</span>
-        </li>
-      </ul>
-
-      <h3 style="margin-top:30px;">2.得分排名</h3>
+      <h3>1.得分排名</h3>
       <ul class="dept-rate">
         <li v-for="({user_name,user_dpt,score,time_length},i) in users" :key="i">
           <span>{{i+1}}.{{user_name}}</span>
           <span>{{user_dpt}}</span>
           <span>{{score}}分</span>
           <span>{{Math.floor(time_length/60)}}分{{time_length%60}}秒</span>
+        </li>
+      </ul>
+      <h3 style="margin-top:30px;">2.各部门平均分与参与率</h3>
+      <ul class="dept-rate">
+        <li v-for="({avg_score,rate,user_dpt},i) in depts" :key="i">
+          <span>{{i+1}}.{{user_dpt}}</span>
+          <span>{{avg_score}}分</span>
+          <span>{{rate}}%</span>
         </li>
       </ul>
     </div>
